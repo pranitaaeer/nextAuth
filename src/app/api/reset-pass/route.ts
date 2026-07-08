1801cae5f56a3cec5b70a5cb08e4a36bef21165f
pranitaaeer <aeerpranita@gmail.com>
2026-07-08 13:07:59 +0530

    implement reset-password route

---
+++ src/app/api/reset-pass/route.ts
@@ -0,0 +1,23 @@
+import { auth } from "@/app/lib/auth";
+import UserModel from "@/app/models/user";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt"
+
+
+export async function POST(request:NextRequest){
+    try {
+        const userId=await auth(request)
+        const user=await UserModel.findById(userId)
+
+        if(!user){
+            return NextResponse.json({error:"user not found"},{status:404})
+        }
+        const {Newpassword}= await request.json()
+        const hashedPassword=await bcrypt.hash(Newpassword,10)
+        user.password=hashedPassword
+
+        return NextResponse.json({message:"Password reset successfully"})
+    } catch (error) {
+        return Response.json({ error: 'error in reset password' }, { status: 500 });
+    }
+}
